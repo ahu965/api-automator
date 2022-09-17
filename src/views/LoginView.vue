aa87c3e21ee3fd8bb5de01e2635f74678f443954
huyanping <>
2022-09-17 11:19:09 +0800

    - 默认样式去除 - 登录页面样式编写 - 新增login路由 - 新增sass模块

---
+++ src/views/LoginView.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="login-container">
+    <a-form
+      class="login-form"
+      :model="formState"
+      name="basic"
+      autocomplete="off"
+    >
+      <div class="login-wrapper">
+        <a-form-item>
+          <h3 class="title">接口测试平台</h3>
+        </a-form-item>
+        <a-form-item
+          name="username"
+          :rules="[{ required: true, message: '请输入用户名！' }]"
+        >
+          <a-input
+            placeholder="请输入用户名"
+            v-model:value="formState.username"
+          />
+        </a-form-item>
+
+        <a-form-item
+          name="password"
+          :rules="[{ required: true, message: '请输入密码!' }]"
+        >
+          <a-input-password
+            placeholder="请输入密码"
+            v-model:value="formState.password"
+          />
+        </a-form-item>
+
+        <a-form-item>
+          <a-button type="primary" class="btn-login">登录</a-button>
+        </a-form-item>
+      </div>
+    </a-form>
+  </div>
+</template>
+
+<script setup>
+import { reactive } from "vue";
+
+const formState = reactive({
+  username: "",
+  password: "",
+});
+</script>
+
+<style lang="scss">
+.login-container {
+  min-height: 100%;
+  width: 100%;
+  background-color: #2d3a4b;
+  overflow: hidden;
+
+  .login-form {
+    width: 520px;
+    margin-top: 10%;
+    margin-left: auto;
+    margin-right: auto;
+
+    .login-wrapper {
+      padding: 25px;
+      border: 1px solid rgba(255, 255, 255, 0.1);
+      background: rgba(0, 0, 0, 0.1);
+      border-radius: 10px;
+    }
+
+    .title {
+      color: #eee;
+      text-align: center;
+      font-size: 32px;
+    }
+
+    #basic_username {
+      height: 45px;
+    }
+
+    .ant-input-password {
+      height: 45px;
+    }
+
+    .btn-login {
+      width: 100%;
+      height: 45px;
+      font-size: 18px;
+    }
+  }
+}
+</style>
